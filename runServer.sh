a64f8f47061690e6e57ce9a24189d64179edc655
ChoiMoonSeok <mon08290@naver.com>
2022-10-06 00:21:27 +0900

    docker 수정

--- runServer.sh
+++ runServer.sh
@@ -1,5 +1,5 @@
 #도커 파일이 위치한 디렉토리에서 실행하세요.
 
 #도커 빌드 및 실행
-sudo docker build -t apiserver:0.0 .
+sudo docker build --no-cache -t apiserver:0.0 .
 sudo docker run -it -p 80:5000 -v $(pwd):/home apiserver:0.0
