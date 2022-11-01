1e153603137cb8beb6e6435482079945dc75b140
ChoiMoonSeok <mon08290@naver.com>
2022-11-01 16:04:33 +0900

    port 번호 수정

--- app.py
+++ app.py
@@ -42,4 +42,4 @@ api.add_namespace(Recommended, '/Recommended')
 api.add_namespace(KeyWords, '/KeyWords')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    app.run(host='0.0.0.0', debug=True)
